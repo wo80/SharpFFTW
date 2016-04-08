995557339665e1132dfa4c10572923665cb5912a
wo80 <christian.woltering@tu-dortmund.de>
2016-04-08 11:05:48 +0200

    Move enums to seperate files

---
+++ FFTWSharp/fftw_direction.cs
@@ -0,0 +1,18 @@
+﻿
+namespace FFTWSharp
+{
+    /// <summary>
+    /// Defines direction of operation
+    /// </summary>
+    public enum fftw_direction : int
+    {
+        /// <summary>
+        /// Computes a regular DFT
+        /// </summary>
+        Forward = -1,
+        /// <summary>
+        /// Computes the inverse DFT
+        /// </summary>
+        Backward = 1
+    }
+}
