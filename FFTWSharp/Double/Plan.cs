752af8bed4b5de9d84fd1560b87471137ae3f49b
wo80 <christian.woltering@tu-dortmund.de>
2016-04-12 17:28:52 +0200

    Cleanup Plan class

--- FFTWSharp/Double/Plan.cs
+++ FFTWSharp/Double/Plan.cs
@@ -17,26 +17,24 @@
 //    * If you have a question about FFTW, ask the FFTW people, and not me. I did not write FFTW.
 //    * If you have a question about this wrapper, probably still don't ask me, since I wrote it almost a decade ago.
 
-using System;
-using System.Runtime.InteropServices;
-using System.Numerics;
-using System.Threading;
-
 namespace FFTWSharp.Double
 {
+    using System;
+    using System.Threading;
+
     /// <summary>
     /// Creates, stores, and destroys fftw plans
     /// </summary>
     public class Plan : AbstractPlan
     {
-        static Mutex FFTW_Lock = new Mutex();
+        private static readonly Mutex mutex = new Mutex();
 
         public Plan(IntPtr handle)
             : base(handle)
         {
         }
 
-        public void Execute()
+        public override void Execute()
         {
             NativeMethods.execute(handle);
         }
@@ -56,170 +54,155 @@ namespace FFTWSharp.Double
         }
 
         #region Plan Creation
+
         //Complex<->Complex transforms
         public static Plan dft_1d(int n, ComplexArray input, ComplexArray output, Direction direction, Options flags)
         {
-            FFTW_Lock.WaitOne();
-
+            mutex.WaitOne();
             var handle = NativeMethods.plan_dft_1d(n, input.Handle, output.Handle, direction, flags);
-            FFTW_Lock.ReleaseMutex();
+            mutex.ReleaseMutex();
 
             return new Plan(handle);
         }
 
         public static Plan dft_2d(int nx, int ny, ComplexArray input, ComplexArray output, Direction direction, Options flags)
         {
-            FFTW_Lock.WaitOne();
-
+            mutex.WaitOne();
             var handle = NativeMethods.plan_dft_2d(nx, ny, input.Handle, output.Handle, direction, flags);
-            FFTW_Lock.ReleaseMutex();
+            mutex.ReleaseMutex();
             
             return new Plan(handle);
         }
 
         public static Plan dft_3d(int nx, int ny, int nz, ComplexArray input, ComplexArray output, Direction direction, Options flags)
         {
-            FFTW_Lock.WaitOne();
-
+            mutex.WaitOne();
             var handle = NativeMethods.plan_dft_3d(nx, ny, nz, input.Handle, output.Handle, direction, flags);
-            FFTW_Lock.ReleaseMutex();
+            mutex.ReleaseMutex();
             
             return new Plan(handle);
         }
 
         public static Plan dft(int rank, int[] n, ComplexArray input, ComplexArray output, Direction direction, Options flags)
         {
-            FFTW_Lock.WaitOne();
-
+            mutex.WaitOne();
             var handle = NativeMethods.plan_dft(rank, n, input.Handle, output.Handle, direction, flags);
-            FFTW_Lock.ReleaseMutex();
+            mutex.ReleaseMutex();
 
             return new Plan(handle);
         }
 
         //Real->Complex transforms
-        public static Plan dft_r2c_1d(int n, ComplexArray input, ComplexArray output, Options flags)
+        public static Plan dft_r2c_1d(int n, RealArray input, ComplexArray output, Options flags)
         {
-            FFTW_Lock.WaitOne();
-
+            mutex.WaitOne();
             var handle = NativeMethods.plan_dft_r2c_1d(n, input.Handle, output.Handle, flags);
-            FFTW_Lock.ReleaseMutex();
+            mutex.ReleaseMutex();
 
             return new Plan(handle);
         }
 
-        public static Plan dft_r2c_2d(int nx, int ny, ComplexArray input, ComplexArray output, Options flags)
+        public static Plan dft_r2c_2d(int nx, int ny, RealArray input, ComplexArray output, Options flags)
         {
-            FFTW_Lock.WaitOne();
-
+            mutex.WaitOne();
             var handle = NativeMethods.plan_dft_r2c_2d(nx, ny, input.Handle, output.Handle, flags);
-            FFTW_Lock.ReleaseMutex();
+            mutex.ReleaseMutex();
 
             return new Plan(handle);
         }
 
-        public static Plan dft_r2c_3d(int nx, int ny, int nz, ComplexArray input, ComplexArray output, Options flags)
+        public static Plan dft_r2c_3d(int nx, int ny, int nz, RealArray input, ComplexArray output, Options flags)
         {
-            FFTW_Lock.WaitOne();
-
+            mutex.WaitOne();
             var handle = NativeMethods.plan_dft_r2c_3d(nx, ny, nz, input.Handle, output.Handle, flags);
-            FFTW_Lock.ReleaseMutex();
+            mutex.ReleaseMutex();
 
             return new Plan(handle);
         }
 
-        public static Plan dft_r2c(int rank, int[] n, ComplexArray input, ComplexArray output, Options flags)
+        public static Plan dft_r2c(int rank, int[] n, RealArray input, ComplexArray output, Options flags)
         {
-            FFTW_Lock.WaitOne();
-
+            mutex.WaitOne();
             var handle = NativeMethods.plan_dft_r2c(rank, n, input.Handle, output.Handle, flags);
-            FFTW_Lock.ReleaseMutex();
+            mutex.ReleaseMutex();
 
             return new Plan(handle);
         }
 
         //Complex->Real
-        public static Plan dft_c2r_1d(int n, ComplexArray input, ComplexArray output, Direction direction, Options flags)
+        public static Plan dft_c2r_1d(int n, ComplexArray input, RealArray output, Options flags)
         {
-            FFTW_Lock.WaitOne();
-
+            mutex.WaitOne();
             var handle = NativeMethods.plan_dft_c2r_1d(n, input.Handle, output.Handle, flags);
-            FFTW_Lock.ReleaseMutex();
+            mutex.ReleaseMutex();
 
             return new Plan(handle);
         }
 
-        public static Plan dft_c2r_2d(int nx, int ny, ComplexArray input, ComplexArray output, Direction direction, Options flags)
+        public static Plan dft_c2r_2d(int nx, int ny, ComplexArray input, RealArray output, Options flags)
         {
-            FFTW_Lock.WaitOne();
-
+            mutex.WaitOne();
             var handle = NativeMethods.plan_dft_c2r_2d(nx, ny, input.Handle, output.Handle, flags);
-            FFTW_Lock.ReleaseMutex();
+            mutex.ReleaseMutex();
 
             return new Plan(handle);
         }
 
-        public static Plan dft_c2r_3d(int nx, int ny, int nz, ComplexArray input, ComplexArray output, Direction direction, Options flags)
+        public static Plan dft_c2r_3d(int nx, int ny, int nz, ComplexArray input, RealArray output, Options flags)
         {
-            FFTW_Lock.WaitOne();
-
+            mutex.WaitOne();
             var handle = NativeMethods.plan_dft_c2r_3d(nx, ny, nz, input.Handle, output.Handle, flags);
-            FFTW_Lock.ReleaseMutex();
+            mutex.ReleaseMutex();
 
             return new Plan(handle);
         }
 
-        public static Plan dft_c2r(int rank, int[] n, ComplexArray input, ComplexArray output, Direction direction, Options flags)
+        public static Plan dft_c2r(int rank, int[] n, ComplexArray input, RealArray output, Options flags)
         {
-            FFTW_Lock.WaitOne();
-
+            mutex.WaitOne();
             var handle = NativeMethods.plan_dft_c2r(rank, n, input.Handle, output.Handle, flags);
-            FFTW_Lock.ReleaseMutex();
+            mutex.ReleaseMutex();
 
             return new Plan(handle);
         }
 
         //Real<->Real
-        public static Plan r2r_1d(int n, ComplexArray input, ComplexArray output, Transform kind, Options flags)
+        public static Plan r2r_1d(int n, RealArray input, RealArray output, Transform kind, Options flags)
         {
-            FFTW_Lock.WaitOne();
-
+            mutex.WaitOne();
             var handle = NativeMethods.plan_r2r_1d(n, input.Handle, output.Handle, kind, flags);
-            FFTW_Lock.ReleaseMutex();
+            mutex.ReleaseMutex();
 
             return new Plan(handle);
         }
 
-        public static Plan r2r_2d(int nx, int ny, ComplexArray input, ComplexArray output, Transform kindx, Transform kindy, Options flags)
+        public static Plan r2r_2d(int nx, int ny, RealArray input, RealArray output, Transform kindx, Transform kindy, Options flags)
         {
-            FFTW_Lock.WaitOne();
-
+            mutex.WaitOne();
             var handle = NativeMethods.plan_r2r_2d(nx, ny, input.Handle, output.Handle, kindx, kindy, flags);
-            FFTW_Lock.ReleaseMutex();
+            mutex.ReleaseMutex();
 
             return new Plan(handle);
         }
 
-        public static Plan r2r_3d(int nx, int ny, int nz, ComplexArray input, ComplexArray output,
+        public static Plan r2r_3d(int nx, int ny, int nz, RealArray input, RealArray output,
             Transform kindx, Transform kindy, Transform kindz, Options flags)
         {
-            FFTW_Lock.WaitOne();
-
+            mutex.WaitOne();
             var handle = NativeMethods.plan_r2r_3d(nx, ny, nz, input.Handle, output.Handle,
                 kindx, kindy, kindz, flags);
-            FFTW_Lock.ReleaseMutex();
+            mutex.ReleaseMutex();
 
             return new Plan(handle);
         }
 
-        public static Plan r2r(int rank, int[] n, ComplexArray input, ComplexArray output,
+        public static Plan r2r(int rank, int[] n, RealArray input, RealArray output,
             Transform[] kind, Options flags)
         {
-            FFTW_Lock.WaitOne();
-
+            mutex.WaitOne();
             var handle = NativeMethods.plan_r2r(rank, n, input.Handle, output.Handle,
                 kind, flags);
-            FFTW_Lock.ReleaseMutex();
+            mutex.ReleaseMutex();
 
             return new Plan(handle);
         }
