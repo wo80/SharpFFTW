909f061b87c40df80e43942e907404ccc9cb15c3
wo80 <christian.woltering@tu-dortmund.de>
2022-11-23 15:03:49 +0100

    Try fixing Ubuntu not finding FFTW libraries (2).

--- SharpFFTW.Tests/Setup.cs
+++ SharpFFTW.Tests/Setup.cs
@@ -1,7 +1,4 @@
 ﻿using NUnit.Framework;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System;
 
 namespace SharpFFTW.Tests
 {
@@ -11,19 +8,7 @@ namespace SharpFFTW.Tests
         [OneTimeSetUp]
         public void GlobalSetup()
         {
-            NativeLibrary.SetDllImportResolver(Assembly.GetExecutingAssembly(), DllImportResolver);
-        }
-
-        private static IntPtr DllImportResolver(string library, Assembly assembly, DllImportSearchPath? searchPath)
-        {
-            // Trying to find FFTW on Ubuntu.
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && library.StartsWith("fftw"))
-            {
-                return NativeLibrary.Load("lib" + library + ".so.3", assembly, searchPath);
-            }
-
-            // Otherwise, fall back to default import resolver.
-            return IntPtr.Zero;
+            Library.SetImportResolver();
         }
     }
 }
